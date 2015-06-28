7b2d6fd64b57579c4a04f7ce54b2cf78b3f05802
Christopher Mckay <chris@error.cm>
2015-06-28 12:36:18 +1000

    head is done

--- src/head.hs
+++ src/head.hs
@@ -1,10 +1,31 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import System.Environment (getArgs)
+import           System.Console.CmdArgs 
+import           System.Environment     (getArgs)
 
 -- head
 main :: IO()
 main = do
-  args <- getArgs
-  let filename = head args
-  putStrLn filename
+  args <- cmdArgs options
+  fileContents <- readFile $ filename args
+  let fileLines = lines fileContents
+  let headLines = take (lineCount args) fileLines
+  putStrLn `mapM_` headLines
+
+data Options = Options {
+  filename :: String,
+  lineCount :: Int
+} deriving (Data, Typeable)
+
+options :: Options
+options = Options
+  { filename = def
+            &= argPos 0
+            &= typ "FILENAME"
+  , lineCount = 10
+             &= explicit &= name "n"
+             &= help "Number of lines to read"
+  }
+  &= summary "A bad clone of head"
+  &= program "head"
