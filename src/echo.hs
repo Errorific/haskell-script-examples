bd1fb574bd6b1030fcdf36037a83c387d09041c4
Christopher Mckay <chris@error.cm>
2015-06-28 11:13:20 +1000

    commented echo

--- src/echo.hs
+++ src/echo.hs
@@ -3,8 +3,12 @@ module Main where
 import           Data.List          (intercalate)
 import           System.Environment (getArgs)
 
+-- echo
 main :: IO()
 main = do
+  -- Get list of args
   args <- getArgs
+  -- do something with the args
   let output = intercalate " " args
+  -- print some output
   putStrLn output
