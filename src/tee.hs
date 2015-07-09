9640872d572dac98664a86bd78410dd6d583b479
Christopher Mckay <chris@error.cm>
2015-07-09 15:33:58 +1000

    tee now uses optparse-applicative

--- src/tee.hs
+++ src/tee.hs
@@ -1,21 +1,21 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import           Control.Monad
-import           System.Console.CmdArgs
+import           Control.Monad          (liftM)
+import           Data.Foldable          (forM_)
 import           System.IO
+import Options.Applicative
 
 -- tee
 main :: IO()
 main = do
-  opts <- cmdArgs options
+  opts <- execParser optsParserInfo
   let fileMode = if append opts then AppendMode else WriteMode
   -- Open all the mentioned output files
   outputFileHandles <- mapM (`openFile` fileMode) $ filenames opts
   -- start reading lines from std in
   stdInLines <- liftM lines getContents
   -- for each line, run hsPutStrLn for stdout and all output files
-  mapM_ (hsPutStrLn (stdout : outputFileHandles)) stdInLines
+  forM_ stdInLines $ hsPutStrLn (stdout : outputFileHandles)
   -- close all the open output files so they flush
   mapM_ hClose outputFileHandles
 
@@ -25,15 +25,20 @@ hsPutStrLn handles = forM_ handles . flip hPutStrLn
 data Options = Options
   { filenames :: [String]
   , append    :: Bool
-  } deriving (Data, Typeable)
+  } deriving (Show)
 
-options :: Options
-options = Options
-  { filenames = def
-             &= args
-             &= typ "FILENAME"
-  , append = False
-          &= help "Append to output file rather than overwrite"
-  }
-  &= summary "A bad clone of tee"
-  &= program "tee"
+optsParser :: Parser Options
+optsParser = Options
+  <$> many (argument str
+    (  metavar "FILENAME..."
+    <> help "Output files"))
+  <*> switch
+    (  long "append"
+    <> short 'a'
+    <> help "Append to output file rather than overwrite")
+
+optsParserInfo :: ParserInfo Options
+optsParserInfo = info (helper <*> optsParser)
+  (  fullDesc
+  <> progDesc "A bad clone of tee"
+  <> header "tee - a bad clone of the real tee")
