1f6211e7fc513f0f7c8cabde2dab9b90557d5319
Christopher Mckay <chris@error.cm>
2015-07-09 15:09:42 +1000

    head uses optparse applicative

--- src/head.hs
+++ src/head.hs
@@ -1,30 +1,35 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import           System.Console.CmdArgs
+import           Data.Foldable       (forM_)
+import           Options.Applicative
 
 -- head
 main :: IO()
 main = do
-  opts <- cmdArgs options
+  opts <- execParser optsParserInfo
   fileContents <- readFile $ filename opts
   let fileLines = lines fileContents
   let headLines = take (lineCount opts) fileLines
-  putStrLn `mapM_` headLines
+  forM_ headLines putStrLn
 
 data Options = Options
   { filename  :: String
   , lineCount :: Int
-  } deriving (Data, Typeable)
+  } deriving (Show)
 
-options :: Options
-options = Options
-  { filename = def
-            &= argPos 0
-            &= typ "FILENAME"
-  , lineCount = 10
-             &= explicit &= name "n"
-             &= help "Number of lines to read"
-  }
-  &= summary "A bad clone of head"
-  &= program "head"
+optsParser :: Parser Options
+optsParser = Options
+  <$> argument str
+    (  metavar "FILENAME"
+    <> help "Input filename")
+  <*> option auto
+    (  short 'n'
+    <> metavar "NUM"
+    <> help "Number of lines to read"
+    <> value 10)
+
+optsParserInfo :: ParserInfo Options
+optsParserInfo = info (helper <*> optsParser)
+  (  fullDesc
+  <> progDesc "A bad clone of head"
+  <> header "head - a bad clone of the real head" )
