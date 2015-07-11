c58748798245d91e5010685966fb39f663a79b82
Christopher Mckay <chris@error.cm>
2015-07-11 10:10:35 +1000

    cat.hs has comments now

--- src/cat.hs
+++ src/cat.hs
@@ -5,7 +5,11 @@ import           System.Environment (getArgs)
 -- cat
 main :: IO()
 main = do
+  -- Get a list of args
   args <- getArgs
+  -- pull the filename out of the args
   let filename = head args
+  -- read the named file
   fileContents <- readFile filename
+  -- print the files contents
   putStrLn fileContents
