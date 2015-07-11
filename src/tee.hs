da573eeb08ae5583091244418553d35851d1960c
Christopher Mckay <chris@error.cm>
2015-07-11 10:24:57 +1000

    tee has comments

--- src/tee.hs
+++ src/tee.hs
@@ -1,14 +1,19 @@
 module Main where
 
-import           Control.Monad       (liftM)
-import           Data.Foldable       (forM_)
-import           Options.Applicative
-import           System.IO
+import Control.Monad       (liftM)
+import Data.Foldable       (forM_)
+import Options.Applicative (Parser, ParserInfo, argument, execParser, fullDesc,
+                            header, help, helper, info, long, many, metavar,
+                            progDesc, short, str, switch, (<*>), (<>))
+import System.IO           (IOMode (AppendMode), IOMode (WriteMode), hClose,
+                            hPutStrLn, openFile, stdout)
 
 -- tee
 main :: IO()
 main = do
+  -- run the parser over the cli argumentts
   opts <- execParser optsParserInfo
+  -- Pick file mode based on option
   let fileMode = if append opts then AppendMode else WriteMode
   -- Open all the mentioned output files
   outputFileHandles <- mapM (`openFile` fileMode) $ filenames opts
@@ -22,11 +27,13 @@ main = do
 -- print a line to all file handles
 hsPutStrLn handles = forM_ handles . flip hPutStrLn
 
+-- structure to hold cli arguments
 data Options = Options
   { filenames :: [String]
   , append    :: Bool
   } deriving (Show)
 
+-- Parser for cli arguments
 optsParser :: Parser Options
 optsParser = Options
   <$> many (
@@ -38,6 +45,7 @@ optsParser = Options
     <> short 'a'
     <> help "Append to output file rather than overwrite")
 
+-- Adding program help text to the parser
 optsParserInfo :: ParserInfo Options
 optsParserInfo = info (helper <*> optsParser)
   (  fullDesc
