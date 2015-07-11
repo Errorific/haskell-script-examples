55f762f473e9973d2cb524f1f9b0f97c9ede51bc
Christopher Mckay <chris@error.cm>
2015-07-11 12:12:38 +1000

    dataimporter is commented

--- src/dataimporter.hs
+++ src/dataimporter.hs
@@ -9,11 +9,16 @@ import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import           Data.Vector                (Vector)
 import qualified Database.PostgreSQL.Simple as Psql
-import           GHC.Generics
-import           Options.Applicative
+import           GHC.Generics               (Generic)
+import           Options.Applicative        (Parser, ParserInfo, argument, auto,
+                                             execParser, fullDesc, header, help,
+                                             helper, info, long, metavar,
+                                             option, progDesc, str, strOption,
+                                             (<*>), (<>))
 import           System.Exit                (exitFailure, exitSuccess)
 import           System.IO                  (hFlush, hSetEcho, stdin, stdout)
 
+-- structure to hold cli arguments
 data Options = Options
   { inputFilename :: String
   , dbHost        :: String
@@ -22,6 +27,7 @@ data Options = Options
   , dbUser        :: String
   } deriving (Show)
 
+-- Parser for cli arguments
 optsParser :: Parser Options
 optsParser = Options
   <$> argument str
@@ -30,21 +36,26 @@ optsParser = Options
     )
   <*> strOption
     (  long "dbhost"
+    <> metavar "HOSTNAME"
     <> help "The hostname of the database"
     )
   <*> option auto
     (  long "dbport"
+    <> metavar "NUMBER"
     <> help "The port of the database"
     )
-  <*> option auto
+  <*> strOption
     (  long "dbname"
+    <> metavar "NAME"
     <> help "The name of the database"
     )
-  <*> option auto
+  <*> strOption
     (  long "dbuser"
+    <> metavar "USERNAME"
     <> help "The user of the database"
     )
 
+-- Adding program help text to the parser
 optsParserInfo :: ParserInfo Options
 optsParserInfo = info (helper <*> optsParser)
   (  fullDesc
@@ -55,42 +66,63 @@ optsParserInfo = info (helper <*> optsParser)
 -- data importer
 main :: IO ()
 main = do
+  -- run the options parser over the cli arguments
   opts <- execParser optsParserInfo
+  -- read in the users password
   dbPass <- getPassword
+  -- read the input files contents
   fileContents <- BSL.readFile $ inputFilename opts
-  let csvContents = readCsv fileContents
+  -- parse the file contents
+  let csvContents = decodeCsv fileContents
+  -- deal with the csv failing to parse
   case csvContents of Left e -> do
+                        -- if it failed, exit like a dirty script
                         putStrLn e
                         exitFailure
                       Right (_, saleRecords) -> do
+                        -- connect to the database
                         conn <- makeConnection opts dbPass
+                        -- insert the sale records into the db
                         _ <- insertSaleRecords conn saleRecords
                         exitSuccess
 
 
+-- capture the password with a blind input
 getPassword :: IO String
 getPassword = do
-  _ <- putStr "password: "
-  _ <- hFlush stdout
-  _ <- hSetEcho stdin False
+  -- output some prompt text
+  putStr "password: "
+  -- flush stdout to make sure the prompt appears
+  -- normally only flushes on new lines or buffer being filled
+  hFlush stdout
+  -- turn off stdin echoing
+  hSetEcho stdin False
+  -- capture 1 line of input, the password
   dbPass <- getLine
-  _ <- hSetEcho stdin True
-  _ <- putStrLn ""
+  -- turn stdin echoing back on
+  hSetEcho stdin True
+  -- print a new line to give the user some feedback
+  putStrLn ""
   return dbPass
 
+-- data type representing our csv rows
 data SaleRecord = SaleRecord
   { item     :: Text
   , quantity :: Int
   , price    :: Double
-  } deriving (Show, Generic)
+  } deriving (Generic)
 
-instance Csv.ToNamedRecord SaleRecord
+-- FromNamedRecord lets cassava read a row matching the column titles to
+-- record element names
 instance Csv.FromNamedRecord SaleRecord
-instance Csv.DefaultOrdered SaleRecord
 
-readCsv :: BSL.ByteString -> Either String (Csv.Header, Vector SaleRecord)
-readCsv fileContents = Csv.decodeByName fileContents
+-- decode the csv files contents
+decodeCsv :: BSL.ByteString -> Either String (Csv.Header, Vector SaleRecord)
+-- decodeByName uses the column headers and FromNamedRecord instance to create
+-- the SaleRecords
+decodeCsv fileContents = Csv.decodeByName fileContents
 
+-- creates a ConnectInfo from our options and then connects to the db with it
 makeConnection :: Options -> String -> IO Psql.Connection
 makeConnection opts pass = Psql.connect $ Psql.ConnectInfo
   (dbHost opts)
@@ -101,12 +133,14 @@ makeConnection opts pass = Psql.connect $ Psql.ConnectInfo
 
 insertSaleRecords :: Psql.Connection -> Vector SaleRecord -> IO (Vector Int64)
 insertSaleRecords conn srs = do
-  _ <- Psql.begin conn
-  rets <- mapM (insertSaleRecord conn) srs
-  _ <- Psql.commit conn
+  -- run inside of a transaction
+  rets <- Psql.withTransaction conn $
+          -- runs an insert statement for each SaleRecord
+          mapM (insertSaleRecord conn) srs
   return rets
 
 insertSaleRecord :: Psql.Connection -> SaleRecord -> IO Int64
 insertSaleRecord conn sr =
+  -- execute a raw sql statement with a list of parameters
   Psql.execute conn "insert into sales (item, quantity, price) values (?, ?, ?)"
     [item sr, T.pack . show $ quantity sr, T.pack . show $ price sr]
