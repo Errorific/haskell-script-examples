5f505d96638747e8ff523324d80cdd11cd4b8a40
Christopher Mckay <chris@error.cm>
2015-07-13 14:02:03 +1000

    acknowledge that head will kill babies

--- src/cat.hs
+++ src/cat.hs
@@ -8,6 +8,8 @@ main = do
   -- Get a list of args
   args <- getArgs
   -- pull the filename out of the args
+  -- Don't use head, it will crash when args is empty
+  --   like the dirtiest of scripts should
   let filename = head args
   -- read the named file
   fileContents <- readFile filename
